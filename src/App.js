9a6e821338aa8a4ee86c1d28191a2e5b93a88d34
Ned <Ned@MacBooks-MacBook-Pro.local>
2023-02-24 12:38:03 +0100

    new project with my react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import HomePage from "./homepage.component";
 
 const App = () => {
   return (
     <div className="App">
-      Hello World
+      <HomePage />
     </div>
   );
-}
+};
 
 export default App;
