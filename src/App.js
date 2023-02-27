84673e86e7822074be9a7a96ad0059658eb98af6
Ned <Ned@MacBooks-MacBook-Pro.local>
2023-02-27 02:24:43 +0100

    new update on e-commerce app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />}></Route>
+        <Route path="/shop" element={<ShopPage/>}></Route>
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
+reportWebVitals();
