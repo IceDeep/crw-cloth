337a473fd1fc2de118d710024eadddb4457afea3
Ned <Ned@MacBooks-MacBook-Pro.local>
2023-02-27 11:26:48 +0100

    just added the header component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
 import ShopPage from "./pages/shop/shop.component";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/header-component/header-component";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />}></Route>
-        <Route path="/shop" element={<ShopPage/>}></Route>
+        <Route path="/shop" element={<ShopPage />}></Route>
       </Routes>
     </div>
   );
